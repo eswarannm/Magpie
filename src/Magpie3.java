403636126f49c95d519fa35334d1ade85177ad4e
Nithya Eswaran <eswarannm@s.dcsdk12.org>
2018-01-23 10:55:32 -0700

    finish activity 4

--- src/Magpie3.java
+++ src/Magpie3.java
@@ -3,15 +3,12 @@
  * <ul>
  * <li>
  * Uses advanced search for keywords</li>
- * <li>
- * Will transform statements as well as react to keywords</li>
  * </ul>
  * 
  * @author Laurie White
  * @version April 2012
- * 
  */
-public class Magpie4 {
+public class Magpie3 {
 	/**
 	 * Get a default greeting
 	 * 
@@ -32,78 +29,28 @@ public class Magpie4 {
 		String response = "";
 		if (statement.length() == 0) {
 			response = "Say something, please.";
-		}
-
-		else if (findKeyword(statement, "no") >= 0) {
+		} else if (findKeyword(statement, "no") >= 0) {
 			response = "Why so negative?";
 		} else if (findKeyword(statement, "mother") >= 0
 				|| findKeyword(statement, "father") >= 0
 				|| findKeyword(statement, "sister") >= 0
 				|| findKeyword(statement, "brother") >= 0) {
 			response = "Tell me more about your family.";
-		}
-
-		// Responses which require transformations
-		else if (findKeyword(statement, "I want", 0) >= 0) {
-			response = transformIWantToStatement(statement);
-		}
-
-		else {
-			// Look for a two word (you <something> me)
-			// pattern
-			int psn = findKeyword(statement, "I", 0);
-
-			if (psn >= 0 && findKeyword(statement, "you", psn) >= 0) {
-				response = transformYouMeStatement(statement);
-			} else {
-				response = getRandomResponse();
-			}
+		} else if (findKeyword(statement, "cat") >= 0
+				|| findKeyword(statement, "dog") >= 0 {
+			response = "Tell me more about your pets.";
+		} else if (findKeyword(statement, "Padjen") >= 0 {
+			response = "He seems like an awesome teacher.";
+		} else if (findKeyword(statement, "school") >= 0 {
+			response = "Oh really! I hate school.";
+		} else if (findKeyword(statement, "cold") >= 0 {
+			response = "I like snow and the chilly weather.";
+		} else if (findKeyword(statement, "life") >= 0 {
+			response = "What is the meaning of life?";
+		} else {
+			response = getRandomResponse();
 		}
 		return response;
-	}
-
-	/**
-	 * Take a statement with "I want to <something>." and transform it into
-	 * "What would it mean to <something>?"
-	 * 
-	 * @param statement
-	 *            the user statement, assumed to contain "I want to"
-	 * @return the transformed statement
-	 */
-	private String transformIWantToStatement(String statement) {
-		// Remove the final period, if there is one
-		statement = statement.trim();
-		String lastChar = statement.substring(statement.length() - 1);
-		if (lastChar.equals(".")) {
-			statement = statement.substring(0, statement.length() - 1);
-		}
-		int psn = findKeyword(statement, "I want", 0);
-		String restOfStatement = statement.substring(psn + 6).trim();
-		return "Would you really be happy if you had " + restOfStatement + "?";
-	}
-
-	/**
-	 * Take a statement with "you <something> me" and transform it into
-	 * "What makes you think that I <something> you?"
-	 * 
-	 * @param statement
-	 *            the user statement, assumed to contain "you" followed by "me"
-	 * @return the transformed statement
-	 */
-	private String transformYouMeStatement(String statement) {
-		// Remove the final period, if there is one
-		statement = statement.trim();
-		String lastChar = statement.substring(statement.length() - 1);
-		if (lastChar.equals(".")) {
-			statement = statement.substring(0, statement.length() - 1);
-		}
-
-		int psnOfYou = findKeyword(statement, "I", 0);
-		int psnOfMe = findKeyword(statement, "you", psnOfYou + 2);
-
-		String restOfStatement = statement.substring(psnOfYou + 2, psnOfMe)
-				.trim();
-		return "Why do you " + restOfStatement + " me?";
 	}
 
 	/**
